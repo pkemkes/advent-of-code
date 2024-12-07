27637c380de07472d7ca90cbac8d20c759ec4377
pkemkes <pkemkes@gmail.com>
2024-12-07 09:05:34 +0100

    Solve 2024/06

--- 2024/06/solution.py
+++ 2024/06/solution.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 def get_guard_pos(route_map: list[str], len_x: int, len_y: int) -> tuple[int, int]:
     for x in range(len_x):
         for y in range(len_y):
@@ -32,6 +34,32 @@ def is_obstacle(pos: tuple[int, int], route_map: list[str], len_x: int, len_y: i
     return get_field_value(pos, route_map, len_x, len_y) == "#"
 
 
+def calc_path_len(route_map: list[str], len_x: int, len_y: int) -> tuple[int, bool]:
+    directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+
+    guard_pos = get_guard_pos(route_map, len_x, len_y)
+    guard_positions = set()
+    current_dir = 0
+    looped = False
+    while is_in_map(guard_pos, len_x, len_y):
+        guard_positions.add((guard_pos, current_dir))
+        while True:
+            next_pos = get_next_pos(guard_pos, directions, current_dir)
+            if not is_obstacle(next_pos, route_map, len_x, len_y):
+                break
+            current_dir = (current_dir + 1) % len(directions)
+        guard_pos = next_pos
+        if (guard_pos, current_dir) in guard_positions: 
+            looped = True
+            break
+    distinct_guard_positions = set(pos for pos, _ in guard_positions)
+
+    return len(distinct_guard_positions), looped
+
+
+# def print_route_map(route_map: list[str])
+
+
 def main():
     with open("input.txt") as f:
         route_map = [l.strip() for l in f.readlines()]
@@ -39,25 +67,23 @@ def main():
     len_x = len(route_map[0])
     len_y = len(route_map)
 
-    directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    path_len, _ = calc_path_len(route_map, len_x, len_y)
 
-    guard_pos = get_guard_pos(route_map, len_x, len_y)
-    guard_positions = []
-    current_dir = 0
-    while is_in_map(guard_pos, len_x, len_y):
-        guard_positions.append(guard_pos)
-        changes = 0
-        while is_obstacle(
-            get_next_pos(guard_pos, directions, current_dir), route_map, len_x, len_y
-        ):
-            current_dir = (current_dir + 1) % len(directions)
-            changes += 1
-            if changes >= len(directions):
-                raise f"Got stuck changing directions at {guard_pos}!"
-        guard_pos = get_next_pos(guard_pos, directions, current_dir)
-    
-    distinct_guard_positions = set(guard_positions)
-    print("Distinct guard positions:", len(distinct_guard_positions))
+    print("Distinct guard positions:", path_len)
+
+    looped_path_count = 0
+    for x in range(len_x):
+        print("working on col", x)
+        for y in range(len_y):
+            if get_field_value((x, y), route_map, len_x, len_y) != ".":
+                continue
+            new_route_map = deepcopy(route_map)
+            new_route_map[y] = new_route_map[y][:x] + "#" + new_route_map[y][x+1:]
+            path_len, looped = calc_path_len(new_route_map, len_x, len_y)
+            if looped:
+                looped_path_count += 1
+
+    print("Number of different positions for looped paths:", looped_path_count)
 
 
 if __name__ == "__main__":
